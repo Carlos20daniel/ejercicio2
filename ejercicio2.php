97adb863498ba77cff2dbcbbed543ab6c27323d1
Carlos20daniel <1440648@senati.pe>
2023-09-29 15:34:28 -0500

    actualizar2

--- ejercicio2.php
+++ ejercicio2.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EJERCICIO 2</title>
-</head>
-<body>
-    <?php 
-    echo "HOLA MUNDO"
-    ?>
-</body>
-</html>
+<?php 
+echo "HOLA MUNDO"
+?>
